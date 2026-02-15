667085a0211ccb75a257d222ca002553378bd8e3
Hulumulu31 <zhuk4702005@gmail.com>
2026-02-15 14:10:36 +0700

    feat: Define RoleAssignment interface (1.5)

---
+++ src/main/java/RoleAssignment.java
@@ -0,0 +1,9 @@
+public interface RoleAssignment {
+    String assignmentId();
+    User user();
+    Role role();
+    AssignmentMetadata metadata();
+    boolean isActive();
+    String assignmentType();
+    String summary();
+}
